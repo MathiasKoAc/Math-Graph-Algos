50cd8cb8c5296d5ec385c64fac1511c3bd76ad32
Mathias Kohs <mathias.kohs@online.de>
2017-04-17 23:59:32 +0200

    Breit Variante1

--- MMI/MMI/CountZhkBreit.cs
+++ MMI/MMI/CountZhkBreit.cs
@@ -15,13 +15,32 @@ namespace MMI
 
         public int CountZhk(Graph Gra, Knoten StartKn)
         {
-            
+            int tagLevel = 0; //entsprich der anz der ZHK
+            StartKn.Tag = tagLevel;
+            foreach (KeyValuePair<int, Knoten> kVP in Gra.Konten)
+            {
+                if (kVP.Value.Tag == -1)
+                {
+                    //neuer Knoten in der Liste -> potentieller neuer ZHK
+                    if (breit(kVP.Value, tagLevel))
+                    {
+                        //deep == true also neues ZHK
+                        tagLevel++;
+                        Console.WriteLine("Breit Neuer Khz");
+                    } else
+                    {
+                        Console.WriteLine("Breit Alter Khz");
+                    }
+                }
+            }
+            return tagLevel;
         }
 
         private bool breit(Knoten kn, int tagLv)
         {
             Queue<Knoten> queue = new Queue<Knoten>();
             queue.Enqueue(kn);
+            bool neuerZHK = true;
 
             //Mache solange bis Warteschlange größer als 0 ist
             while (queue.Count > 0)
@@ -31,18 +50,19 @@ namespace MMI
                 if (knoten.Tag == -1)
                 {
                     knoten.Tag = tagLv;
-
+                    Console.WriteLine("Knoten: " + knoten.Wert);
                     foreach (var kante in kn.Kanten)
                     {
-                        if (knoten.Tag == -1)
-                        {
-                            queue.Enqueue(kante.ToKnoten);
-                        }                            
+                        queue.Enqueue(kante.ToKnoten);                 
                     }
+                } else if (knoten.Tag < tagLv)
+                {
+                    neuerZHK = false;
                 }
+                //Console.WriteLine(queue.Count);
             }
-            ///___________________________
-            return false;
+            
+            return neuerZHK;
         }
     }
 }
