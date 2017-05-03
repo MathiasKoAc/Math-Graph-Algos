75e54a9034ea1184a6415fb695a6fd53abf540ee
Mathias Kohs <mathias.kohs@online.de>
2017-05-03 09:41:11 +0200

    Kruskal code AddKante

--- MMI/MMI/Algos/Kruskal.cs
+++ MMI/MMI/Algos/Kruskal.cs
@@ -10,12 +10,59 @@ namespace MMI.Algos
     {
         public int CountMST(Graph Gra)
         {
-            throw new NotImplementedException();
+            int maxTag = 0;
+            List<Kante> Kanten = new List<Kante>();
         }
 
-        private bool kreisFrei()
+        private bool addKante(ref Kante k, ref List<Kante> Kanten, ref int maxTag)
         {
+            bool added = false;
+            Knoten toKnoten = k.ToKnoten;
+            Knoten fromKnoten = k.FromKnoten;
 
+
+            //neue Kante
+            if (toKnoten.Tag == -1 || fromKnoten.Tag == -1)
+            {
+                toKnoten.Tag = maxTag;
+                fromKnoten.Tag = maxTag;
+                maxTag++;
+                Kanten.Add(k);
+                added = true;
+            }
+            else if (toKnoten.Tag > fromKnoten.Tag)
+            {
+                // andere alle Knoten mit dem toTag auf das Tag vom fromKnoten
+                changeKanteTag(ref Kanten, toKnoten.Tag, fromKnoten.Tag);
+                Kanten.Add(k);
+                added = true;
+            }
+            else if (toKnoten.Tag < fromKnoten.Tag)
+            {
+                // andere alle Knoten mit dem fromTag auf das Tag vom toKnoten
+                changeKanteTag(ref Kanten, fromKnoten.Tag, toKnoten.Tag);
+                Kanten.Add(k);
+                added = true;
+            }
+
+            return added;
+        }
+
+        /// <summary>
+        /// Aendert das Tag der Kanten in der List mit dem Tag fromTag auf den Wert toTag
+        /// </summary>
+        /// <param name="Kanten">ref List von Kanten</param>
+        /// <param name="fromTag">zu suchender Tag</param>
+        /// <param name="toTag">Tag auf den geandert werden soll</param>
+        private void changeKanteTag(ref List<Kante> Kanten, int fromTag, int toTag)
+        {
+            foreach(Kante kant in Kanten)
+            {
+                if(kant.Tag == fromTag)
+                {
+                    kant.Tag = toTag;
+                }
+            }
         }
     }
 }
