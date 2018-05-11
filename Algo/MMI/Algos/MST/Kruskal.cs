ff6556eb280020a82f7e1369fc05fba6e534b27e
Mathias Kohs <mathias.kohs@online.de>
2018-05-11 11:59:53 +0200

    Add: MST Variante aus altem Semester

---
+++ Algo/MMI/Algos/MST/Kruskal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMI.Algos
+{
+    class Kruskal : AbsMST
+    {
+        public override double CountMST(Graph Gra, out List<Kante> Kanten)
+        {
+            int maxTag = 0;
+            Kanten = new List<Kante>();
+            double mstSize = 0;
+
+            foreach(Kante k in Gra.Kanten)
+            {
+                mstSize += addKante(k, ref Kanten, ref maxTag);
+            }
+            return mstSize;
+        }        
+    }
+}
---
+++ Algo/MMI/Algos/MST/Prim.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMI.Algos
+{
+    class Prim : AbsMST
+    {
+
+        public override double CountMST(Graph Gra, out List<Kante> Kanten)
+        {
+            return CountMST(Gra, out Kanten, Gra.Knoten[0]);
+        }
+
+        public double CountMST(Graph Gra, out List<Kante> ZielKanten, Knoten startKnoten)
+        {
+            int knotenMaxAnz = Gra.Knoten.Count;
+            int knotenCount = 0;
+
+            ZielKanten = new List<Kante>();
+            List<Kante> umgebungsKanten = new List<Kante>();
+
+            int maxTag = 0;
+            double mstSize = 0;
+            double tmpMstSize = 0;
+            Kante focusKante;
+
+            //starten...
+            addKantenVonKnoten(startKnoten, ref umgebungsKanten);
+
+            do
+            {
+                focusKante = pullKante(ref umgebungsKanten);
+                if (focusKante != null)
+                {
+
+                    tmpMstSize = addKante(focusKante, ref ZielKanten, ref maxTag);
+                    if (tmpMstSize > 0f)
+                    {
+                        knotenCount++;
+                        mstSize += tmpMstSize;
+                        addKantenVonKnoten(focusKante.ToKnoten, ref umgebungsKanten);
+                        addKantenVonKnoten(focusKante.FromKnoten, ref umgebungsKanten);
+                    }
+                }
+
+            } while (knotenCount < knotenMaxAnz && focusKante != null);
+
+            return mstSize;
+        }
+
+        private Kante pullKante(ref List<Kante> sortSet)
+        {
+            if(sortSet.Count > 0)
+            {
+                Kante focusKante = sortSet.Min();
+                sortSet.Remove(focusKante);
+                return focusKante;
+            }
+            return null;
+        }
+
+        private void addKantenVonKnoten(Knoten knot, ref List<Kante> sortSet)
+        {
+            foreach (Kante kant in knot.Kanten)
+            {
+                if(kant.ToKnoten.Tag == -1 || kant.FromKnoten.Tag == -1)
+                {
+                    sortSet.Add(kant);
+                }                
+            }
+        }
+    }
+}
